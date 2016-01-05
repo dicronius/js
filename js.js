a2fa88819c5361364f13f87a2d6924c09c399f9d
dicronius <davidmmsilva1997@gmail.com>
2016-01-05 09:13:03 +0000

    Update js.js

--- js.js
+++ js.js
@@ -6,10 +6,14 @@ $.ajax({
     type: "GET",
     url: "https://cdn.rawgit.com/dicronius/js/master/xml.xml",
     dataType: "xml",
-    success: function (xml) {}
-});
-
-$(xml).find('cofinaeditor/Articles').each(function () {
+    success: function (xml) {
+        
+    $(xml).find('cofinaeditor/Articles').each(function () {
     var Titles = $(this).find('Title').text();
     $("<li></li>").html(Titles).appendTo("#titulos ul");
+     
+    });
+        
+    }
+    
 });
