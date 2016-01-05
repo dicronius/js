aeb71a5b95b7275e82fbcd4f183f63245a83e256
dicronius <davidmmsilva1997@gmail.com>
2016-01-05 10:38:22 +0000

    Update js.js

--- js.js
+++ js.js
@@ -1,18 +1,22 @@
-$(document).ready(function () {
-    //the variable to be appended here
-    $("#titulos").append("<ul></ul>");
-});
-$.ajax({
     type: "GET",
     url: "https://cdn.rawgit.com/dicronius/js/master/xml.xml",
     dataType: "xml",
     success: function (xml) {
         
-    $(xml).find("Title").each(function () {
-    var Titles = $(this).text();
-    $("<li></li>").html(Titles).appendTo("#titulos ul");
-     
-    });
+        $(xml).find("Articles").each(function () {
+            var Titles = $("title").text();           
+
+            var socket = io();
+            $('form').submit(function () {
+                socket.emit('chat message', $(Titles).val());
+                return false;
+            });
+
+            socket.on('chat message', function (msg) {
+                $('#titulos').append($('<li>').text(msg));
+            });
+
+        });
         
     }
     
